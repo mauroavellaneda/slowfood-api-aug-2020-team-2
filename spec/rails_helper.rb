96a71e65004032dab0f5c9d0c7ad103bc3dc2b81
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-09-21 16:30:28 +0200

    Setting up configuration for Rspec. Cleanning up unnecessary coments. Setting up Coveralls for Rspec.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,22 +1,31 @@
+require 'spec_helper'
 require 'coveralls'
 Coveralls.wear_merged!('rails')
-
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
 
-abort('The Rails environment is running in production mode!') if Rails.env.production?
-require 'spec_helper'
+require File.expand_path('../config/environment', __dir__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
-ActiveRecord::Migration.maintain_test_schema!
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+
   config.use_transactional_fixtures = true
+
+
   config.infer_spec_type_from_file_location!
+
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+
 end
